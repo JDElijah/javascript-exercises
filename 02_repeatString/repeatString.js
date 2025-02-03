82c027cd56f0aeb585ed1e3210c6012fc93a5d04
JDElijah <139030487+JDElijah@users.noreply.github.com>
2025-02-03 09:42:19 -0800

    02-repeatstrig-solution
    For loop used to contatenate additional copies of the provided string to the output.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+   let output = ""; 
+   for (let i = 0; i < num; i ++) {
+	output = output + text; 
+   }
+   return output;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
