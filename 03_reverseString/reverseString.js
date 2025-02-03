84c4dbcc8ece52508cd0c2d39fe0b3065ce9c846
JDElijah <139030487+JDElijah@users.noreply.github.com>
2025-02-03 10:05:20 -0800

    03-reversestring solution
    For loop to iterate through the given string in reverse. enabled skipped tests for full testing.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(txt) {
+    let strResult = ""; 
+    let len = txt.length * (-1); 
+    for (let i = -1; i >= len; i--)
+    {
+        strResult += txt.at(i);
+    }
+    return strResult; 
 };
 
 // Do not edit below this line
