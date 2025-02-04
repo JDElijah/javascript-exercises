f40bf79b5f019c4a411aa3396e8b1bebae2f2ebe
JDElijah <139030487+JDElijah@users.noreply.github.com>
2025-02-04 08:20:33 -0800

    04-removefromarray solution
    array filter to return an an array wihtout the givem ...args.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    return arr.filter(val => !args.includes(val));
+    /* The filter() method creates a new arrayy filled with elements that pass a test provided by a function
+        The filter() method does not execute the function for empty elements. 
+        The filter() method does not change the original array. 
+     */
+
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+const removeFromArrayV2 = function(arr, ...args) {
+    const newArray = []; 
+    arr.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    })
+    return newArray;
+}
