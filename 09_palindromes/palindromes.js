42f0397bfcfaa0e5f93733d4ef5b2a272ff9f8e7
JDElijah <139030487+JDElijah@users.noreply.github.com>
2025-02-04 17:55:55 -0800

    09-palindromes solution
    Considering alphanumeric values only. Convert the input string to only those values without spaces. Create a new string that is the reversed of this pure string. Finally, compare the two strings to check if palindrome.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+    // Consider only letterrs and numbers in a variable. 
+    const alphanumerical ='abcdefghijklmnopqrstuvwxyz0123456789';
+
+    // Convert the input string to lowercase, split to array of individual characters. 
+    const arrayStr = inputString
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join(''); 
+    
+    // Create a new reversed string for comparison
+    const reversedString = arrayStr.split('').reverse().join(''); 
+
+    // Return the outcome of the comparison between the arrayStr and reversedString.
+    return arrayStr === reversedString;
 
 };
 
