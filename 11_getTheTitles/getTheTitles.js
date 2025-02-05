2707d6c0a9920e81f823d719b65c7e005ca572f9
JDElijah <139030487+JDElijah@users.noreply.github.com>
2025-02-04 18:40:04 -0800

    11-getTheTitles solution
    using the array map feature to create a new array with an arrow function to extract book titles.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookDict) {
+    return bookDict.map((book) => book.title); 
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+/**
+ * The Map object holds key-value pairs and remembers the original insertion order
+ * of keys. Any value (both objects and primitive values) may be used as either a key or value. 
+ */
